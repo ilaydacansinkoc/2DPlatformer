9523cb1056cc9e0ac0e0b0ed35fc447e6fde3f03
ilaydacansinkoc <ilaydacansin@gmail.com>
2017-08-03 02:14:59 +0300

    Title Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+	public string loadLevel;
+	public string levelSelect;
+
+	public void loadDesiredLevel(){
+		SceneManager.LoadScene (loadLevel);
+	}
+
+	public void selectLevel(){
+		SceneManager.LoadScene (levelSelect);
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+}
