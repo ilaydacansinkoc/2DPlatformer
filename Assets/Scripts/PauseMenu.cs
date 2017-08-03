5030ef436f34807dc482007671ab5bcacbeb2c38
ilaydacansinkoc <ilaydacansin@gmail.com>
2017-08-03 03:28:50 +0300

    In-Game Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public string loadLevel;
+	public string levelLoader;
+
+	public GameObject canvas;
+	public bool isResumed;
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isResumed = !isResumed;
+		}
+
+		if (isResumed) {
+			
+			canvas.SetActive (true);
+			Time.timeScale = 0f;
+
+		} else {
+			canvas.SetActive (false);
+			Time.timeScale = 1f;
+		}
+
+
+	}
+
+
+	public void Resume(){
+
+		isResumed = true;
+		
+	}
+	public void selectLevel(){
+		SceneManager.LoadScene (levelLoader);
+	}
+	public void ReturnMenu(){
+		SceneManager.LoadScene (loadLevel);
+	}
+}
